43a5fc589d1875a722c3f2d706c9a11b0f990e19
steven-yn <87164509+steven-yn@users.noreply.github.com>
2022-01-02 21:40:34 +0900

    Updates

---
+++ precache-manifest.d2cd6a2add5564b3f2649dea1e8d32c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9547f219ac13a7ca6f4e57d39282869b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b228897816891065145b",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "58414b1dd94bb00c99a2",
+    "url": "/static/js/2.4225d5fc.chunk.js"
+  },
+  {
+    "revision": "b228897816891065145b",
+    "url": "/static/js/main.9a488f85.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
